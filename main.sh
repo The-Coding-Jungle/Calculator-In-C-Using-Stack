91b509cc84bddd57322ece4cc6c8c1973bd63560
HetDaftary <hetdaftary@gmail.com>
2021-12-29 23:33:09 +0530

    Making minor changes

--- main.sh
+++ main.sh
@@ -1,9 +1,9 @@
-if [ ! -f build ] ; then
-    mkdir build
+if [ ! -d "build" ] ; then
+    mkdir build;
 fi
 
-if [ ! -f data ] ; then
-    mkdir data
+if [ ! -d "data" ] ; then
+    mkdir data;
 fi
 
 gcc src/main.c -o build/main.out -lm 
